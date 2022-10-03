3dada92d7e188fea3b49bfcabbdc977247886477
ismailD <issmailduman@gmail.com>
2022-10-04 00:30:18 +0200

    SeleniumMethods

--- src/test/java/Day1/SeleniumMethods.java
+++ src/test/java/Day1/SeleniumMethods.java
@@ -1,4 +1,24 @@
 package Day1;
 
+<<<<<<< HEAD
 public class SeleniumMethods {
+=======
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumMethods {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+
+        driver.get("https://www.google.de/?hl=de");
+        driver.manage().window().maximize();
+
+
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        driver.close();
+    }
+>>>>>>> 8becd25 (SeleniumMethods)
 }
